8f1b663f6378c10b2595da50dc681a0c1ed2c310
Lesio Programista <lama7410@gmail.com>
2020-08-11 22:08:43 +0200

    #init

---
+++ StudentTests.Api/Startup.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+//using RoomMate.Domain;
+//using RoomMate.Domain.Services.Implements;
+//using RoomMate.Domain.Services.Interfaces;
+//using RoomMate.Repository;
+using SimpleInjector;
+using SimpleInjector.Integration.AspNetCore.Mvc;
+using SimpleInjector.Lifestyles;
+using System;
+using System.Text;
+
+namespace RoomMate.Api
+{
+    public class Startup
+    {
+        private Container container = new Container();
+
+        public Startup(IHostingEnvironment env)
+        {
+            // ASP.NET default stuff here
+        }
+
+        // This method gets called by the runtime.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDistributedMemoryCache();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddSession(options =>
+            {
+                // Set a short timeout for easy testing.
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                // Make the session cookie essential
+                options.Cookie.IsEssential = true;
+            });
+
+            services.AddCors(options => options.AddPolicy("Cors", builder =>
+            {
+                builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            }));
+
+            services.AddAuthentication(opt => {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("StachuLesiuProgramista@345"))
+                };
+                options.Events = new JwtBearerEvents()
+                {
+                    OnAuthenticationFailed = c =>
+                    {
+                        c.NoResult();
+
+                        c.Response.StatusCode = 401;
+                        c.Response.ContentType = "text/plain";
+
+                        return c.Response.WriteAsync(c.Exception.ToString());
+                    }
+                };
+            });
+
+            services.AddMvc();
+
+            IntegrateSimpleInjector(services);
+        }
+
+        private void IntegrateSimpleInjector(IServiceCollection services)
+        {
+            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddSingleton<IControllerActivator>(
+                new SimpleInjectorControllerActivator(container));
+            services.AddSingleton<IViewComponentActivator>(
+                new SimpleInjectorViewComponentActivator(container));
+
+            services.EnableSimpleInjectorCrossWiring(container);
+            services.UseSimpleInjectorAspNetRequestScoping(container);
+        }
+
+        // Configure is called after ConfigureServices is called.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            InitializeContainer(app);
+            container.Verify();
+            app.UseCors("Cors");
+            app.UseCors(policy =>
+            {
+                policy.AllowAnyHeader();
+                policy.AllowAnyMethod();
+                policy.AllowAnyOrigin();
+            });
+
+            app.UseAuthentication();
+
+            app.UseSession();
+            app.UseMvc();
+        }
+
+        private void InitializeContainer(IApplicationBuilder app)
+        {
+            //// Add application presentation components:
+            container.RegisterMvcControllers(app);
+            container.RegisterMvcViewComponents(app);
+
+            //// Add application services. For instance:
+            //container.Register<RoomMateContext>(Lifestyle.Scoped);
+            //container.Register<DbContext, RoomMateContext>(Lifestyle.Scoped);
+            //container.Register(typeof(IRepository<>), typeof(Repository<>), Lifestyle.Scoped);
+
+
+            ////// Tutaj dodawać nowe serwisy
+            ///// TODO: Dodać automat na to, żeby przeszło klasy dziedziczące po Serwisie
+            //container.Register<ITokenService, TokenService>(Lifestyle.Scoped);
+            ////container.Register<IFlatService, FlatService>(Lifestyle.Scoped);
+            //container.Register<IHouseworkService, HouseworkService>(Lifestyle.Scoped);
+            //container.Register<ILoginService, LoginService>(Lifestyle.Scoped);
+            //container.Register<IRegisterService, RegisterService>(Lifestyle.Scoped);
+            //container.Register<IUserService, UserService>(Lifestyle.Scoped);
+            //container.Register<IAddressService, AddressService>(Lifestyle.Scoped); //addr serv
+
+
+            //// Allow Simple Injector to resolve services from ASP.NET Core.
+            //container.AutoCrossWireAspNetComponents(app);
+        }
+    }
+}
---
+++ StudentTests.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace StudentTests.Repository
+{
+    public class Class1
+    {
+    }
+}
